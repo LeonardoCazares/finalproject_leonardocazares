e721c9433694112c0863626b09b7e77dd5defe7e
Leonardo Damián Cázares Trejo <leonardocazares@tamu.edu>
2025-11-16 01:44:09 -0600

    Build train and validation sets.

--- R/cross_validation.R
+++ R/cross_validation.R
@@ -15,28 +15,28 @@ cv_dataset_creation <- function(x, folds = 5) {
   cv_list <- vector("list", folds)
   names(cv_list) <- paste0("fold_", seq_len(folds))
 
-  #for (i in seq_len(folds)) {
-#
-  #  # Index for the validation fold
-  #  start_val <- (i - 1L) * n_per_fold + 1L
-  #  # Last fold holds the remaining part
-  #  end_val <- if (i < folds) i * n_per_fold else n
-#
-  #  # Training before validation block
-  #  train_1 <- if (start_val > 1L) x[1:(start_val - 1L)] else x[integer(0)]
-#
-  #  # Training after validation block
-  #  train_2 <- if (end_val < n) x[(end_val + 1L):n] else x[integer(0)]
-#
-  #  # Validation block
-  #  val <- x[start_val:end_val]
-#
-  #  cv_list[[i]] <- list(
-  #    train_batch_1 = train_1,
-  #    val           = val,
-  #    train_batch_2 = train_2
-  #  )
-  #}
-#
-  #return(cv_list)
+  for (i in seq_len(folds)) {
+
+    # Index for the validation fold
+    start_val <- (i - 1L) * n_per_fold + 1L
+    # Last fold holds the remaining part
+    end_val <- if (i < folds) i * n_per_fold else n
+
+    # Training before validation block
+    train_1 <- if (start_val > 1L) x[1:(start_val - 1L)] else x[integer(0)]
+
+    # Training after validation block
+    train_2 <- if (end_val < n) x[(end_val + 1L):n] else x[integer(0)]
+
+    # Validation block
+    val <- x[start_val:end_val]
+
+    cv_list[[i]] <- list(
+      train_batch_1 = train_1,
+      val           = val,
+      train_batch_2 = train_2
+    )
+  }
+
+  return(cv_list)
 }
