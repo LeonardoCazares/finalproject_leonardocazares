feac492ac7180c0262090e78aae6c014fb7fc5ea
Leonardo Damián Cázares Trejo <leonardocazares@tamu.edu>
2025-11-15 22:35:09 -0600

    Correct column name.

--- R/temp_austin-data.R
+++ R/temp_austin-data.R
@@ -4,7 +4,7 @@
 #'
 #' @format A data frame with 3557 rows and the daily temperature measurement for Austin (on average humidity conditions):
 #' \describe{
-#'   \item{datetime}{Date YYYY-MM-DD}
+#'   \item{dates}{Date YYYY-MM-DD}
 #'   \item{temp}{numeric, Temperature on average humidity conditions (°F)}
 #' }
 #' @source \url{https://www.ncei.noaa.gov/} (NCEI / NOAA Climate Data Online)
