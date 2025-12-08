2fd7653f9d2bcd776d2fa7805472787ae7026aea
Leonardo Damián Cázares Trejo <leonardocazares@tamu.edu>
2025-12-07 19:06:30 -0600

    Correct comments and variable names.

--- R/dynamic_regression.R
+++ R/dynamic_regression.R
@@ -23,6 +23,12 @@
 #' print(x_test)
 #' print(out$y_pred)
 dynamic_regression <- function(x_train, x_test, beta, lags) {
+
+  # Lags check
+  if (length(x_train) <= lags) {
+    stop("`lags` must be smaller than length(x_train).")
+  }
+
   # One prediction per test point
   length_test <- length(x_test)
 
@@ -33,13 +39,13 @@ dynamic_regression <- function(x_train, x_test, beta, lags) {
   x_train_dynamic <- x_train
   lenght_dynamic_train <- length(x_train_dynamic)
 
-  for (step in 1:length_test) {
+  for (step in seq_len(length_test)) {
     # Fit the ridge regression in the current dynamic training step
     out_dynamic <- fit_ar_l2(x_train_dynamic, lags, beta)
-    coef_dynamic <- as.vector(out_dynamic$coef)
+    coef_dynamic <- as.numeric(out_dynamic$coef)
 
     # Get the out-of-sample prediction given the current x_train_dynamic
-    x_train_last_window <- as.vector(x_train_dynamic[(lenght_dynamic_train - lags + 1):lenght_dynamic_train])
+    x_train_last_window <- as.vector(x_train_dynamic[(lenght_dynamic_train - lags + 1):(lenght_dynamic_train)])
     y_hat <- sum(coef_dynamic * x_train_last_window)
     y_pred[step] <- y_hat
 
